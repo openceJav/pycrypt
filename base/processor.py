ac767f3ee4de72ba59c7705d7e2bba64e567746a
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-05-11 00:36:08 -0400

    add: /data processor.py refactor: main.py requirements.txt

---
+++ base/processor.py
@@ -0,0 +1,6 @@
+# TODO: Processor to Process Data and Generate Clean Data
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
--- main.py
+++ main.py
@@ -11,3 +11,25 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sb
+
+# Rich Lib
+from rich import print, box
+from rich.console import Console
+from rich.table import Table
+from rich.progress import Progress
+
+# Inquirer Lib
+from inquirer import prompt, List, Checkbox, Text, Confirm
+
+# PyTorch Lib
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+# TODO: Start PyCrypt Project
+
+
+# TODO: Complete PyTorch Model
+# TODO: Complete PyTorch Training
+# TODO: Complete PyTorch Testing
+# TODO: Complete PyTorch Prediction
