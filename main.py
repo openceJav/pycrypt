81359b583c67be283441e43fd46c2afdee327b75
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-05-10 23:23:19 -0400

    refactor: main.py requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+# https://learning.oreilly.com/videos/pytorch-ultimate-2024/9781801070089/9781801070089-video1_1/
+
+import sys
 import os
 import re
-import base
-import uuid
+import json
+import requests as req
+import datetime as dt
 
-# TODO: Create PyCrypt! (Python General Prediction CLI Application)
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
