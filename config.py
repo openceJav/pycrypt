75aac6aa8dd3d2831b31c1b1d0008462dff0361d
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-05-21 14:07:02 -0400

    refactor: .env, config.py

--- config.py
+++ config.py
@@ -1,2 +1,13 @@
 import os
 from dotenv import load_dotenv
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Load .env File
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+TOP_LEVEL_DIR = os.path.abspath(os.curdir)
+
+
+class Config(object):
+    DEBUG = False
